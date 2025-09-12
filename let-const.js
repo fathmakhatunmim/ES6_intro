5229ce90a7ea77c41f6e5c64b59ecd9e6aa9c043
Fathma Khatun Mim <fathmakhatunmim@gmail.com>
2025-09-12 21:20:57 +0600

    secound

---
+++ let-const.js
@@ -0,0 +1,23 @@
+//es6
+//const diye value change korte parbo na
+
+const name = 'Bangladesh';
+
+const countryName = 'BD' + name;
+console.log(countryName)
+
+//let diye value change kora jai
+
+let price = 120;
+price = 150;
+console.log(price)
+
+//var
+var device = 'laptop';
+console.log(device)
+
+const dress = ['shirt', 'pant', 'jacket'];
+dress.push('maflar')
+console.log(dress)
+
+const student = {name: 'jaglul' , marks:50}
