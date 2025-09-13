063ce1e2fec175e81e594f3654f2f390afa66c8c
Fathma Khatun Mim <fathmakhatunmim@gmail.com>
2025-09-13 21:21:57 +0600

    destrucing

---
+++ destructuring.js
@@ -0,0 +1,30 @@
+// const product = {name:'shirt', price:500,quantity:7}
+
+const {price} = {name:'shirt', price:500,quantity:7}
+
+console.log(price)
+
+
+
+
+// const discount = product.price*/100;
+// const yourPay = product.price - discount;
+// const vatAmount = product.price*100;
+// const totalAmount = yourPay + vatAmount;
+
+
+
+// const price = product.price;
+
+// const discount = price*20/ 100;
+// const yourPay = price - discount;
+// const vatAmount = price*100;
+// const totalAmount = yourPay + vatAmount;
+
+
+
+//-----------------------------------array----------
+const numbers =[25,88]
+const[first,second] = numbers
+const[math,physics]=[90,99];
+console.log(physics)
